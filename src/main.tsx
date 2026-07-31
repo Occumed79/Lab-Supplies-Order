5011f3791d118933042981a7a3a849dad376c79b
Occumed79 <Alex.Ayvazian@logan.edu>
2026-07-31 15:07:45 -0700

    Load the approved clinic order card behavior

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 import './portal-theme-overrides.css'
+import './clinic-order-enhancements'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
