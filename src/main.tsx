81fb567f2f20895e9b97270843b992ec4f0338f3
Occumed79 <Alex.Ayvazian@logan.edu>
2026-07-31 13:53:55 -0700

    Load the restored floating glass portal shell

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
+import './portal-shell.css'
 import './clinic-order-enhancements'
+import './portal-shell'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
