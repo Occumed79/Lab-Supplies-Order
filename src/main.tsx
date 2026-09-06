9e0953546549a01e37d4e4ccc1757d27b06e5c54
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-06 16:52:53 -0700

    Load isolated luminous navy login theme

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 import './portal-shell.css'
+import './login-theme.css'
 import './clinic-order-enhancements'
 import './portal-shell'
 
